8408d0f941b19c22b570f704831170d874fa7436
gaksss <clement.carlier2@gmail.com>
2024-11-20 12:25:09 +0100

    exo3 fini

---
+++ EXERCICE3/script.js
@@ -0,0 +1,26 @@
+const listener = document.querySelector("body");
+const para = document.createElement("p");
+const texte = document.createTextNode("BRAVOOOOOOO");
+para.appendChild(texte);
+let combi = []; // Initialisation de la combinaison de touches
+
+listener.addEventListener("keyup", handleKeyDown);
+
+function handleKeyDown(event) {
+  if (["m", "c"].includes(event.key) && !combi.includes(event.key)) {
+    combi.push(event.key); // Ajoute la touche si elle n'est pas déjà dans la combinaison
+  }
+
+  // Vérifie si la combinaison "m" et "c" est pressée
+  if (combi.sort().join("") === "cm") {
+    // Si le paragraphe n'est pas encore ajouté, on l'ajoute
+    if (!document.body.contains(para)) {
+      listener.appendChild(para);
+    }
+  }
+
+  // Si une troisième touche est pressée (par exemple, "t"), change la couleur
+  if (event.key === "r") {
+    para.classList.add("red");
+  }
+}
